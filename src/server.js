c945896470ed51340dc60b02b4b8bf8a1f59e118
Sandeep <sandeepsoni@excellencetechnologies.in>
2022-07-15 15:54:04 +0530

    all done

--- src/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express"); 
-const firebaseApp = require("./firebase.config");
-const app = express(); 
-app.use(express.json()); 
-function main(){
-    app.get("/", (req, res)=>res.send("ok"));
-}
-app.listen(8000, main);
